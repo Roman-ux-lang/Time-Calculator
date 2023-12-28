e76c133eb7140b30b6263c818608454b7b891a1c
Roman-ux-lang <114356774+Roman-ux-lang@users.noreply.github.com>
2023-12-27 20:14:32 -0600

    Update time_Calculator.py

--- time_Calculator.py
+++ time_Calculator.py
@@ -110,27 +110,3 @@ print(add_time("11:43 PM", "24:20", "Tuesday"))
 print(add_time("6:30 PM", "205:12"))
 # Returns: 7:42 AM (9 days later)
 
-print(add_time("3:30 PM", "2:12"))
-
-print(add_time("11:55 AM", "3:12"))
-
-print(add_time("9:15 PM", "5:30"))
-
-print(add_time("11:40 AM", "0:25"))
-
-print(add_time("2:59 AM", "24:00"))
-
-print(add_time("11:59 PM", "24:05"))
-
-print(add_time("8:16 PM", "466:02"))
-
-print(add_time("5:01 AM", "0:00"))
-
-print(add_time("3:30 PM", "2:12", "Monday"))
-
-print(add_time("2:59 AM", "24:00", "saturDay"))
-
-print(add_time("11:59 PM", "24:05", "Wednesday"))
-
-
-print(add_time("8:16 PM", "466:02", "tuesday"))
